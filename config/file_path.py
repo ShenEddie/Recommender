ed8ce57adbb811013088163531271d98fc59896a
Eddie Shen <sheneddie@outlook.com>
2021-09-24 11:38:39 +0800

    Config file path

---
+++ config/file_path.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# @Time    : 2021-09-24 10:58
+# @Author  : Eddie Shen
+# @Email   : sheneddie@outlook.com
+# @File    : file_path.py
+# @Software: PyCharm
+
+ml_1m_ratings_path = r"D:\Eddie\Documents\BOSC\RecommendSystem\data\ml-1m\ratings.dat"
