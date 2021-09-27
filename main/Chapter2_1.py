64de430ab88d48c30463036b50875ac300365edb
Eddie Shen <sheneddie@outlook.com>
2021-09-27 10:54:33 +0800

    Add item similarity

--- main/Chapter2_1.py
+++ main/Chapter2_1.py
@@ -16,6 +16,7 @@ import random
 from typing import List, Dict
 from operator import itemgetter
 from tqdm import tqdm
+from pprint import pprint
 
 sys.path.append('../')
 try:
@@ -44,3 +45,42 @@ test_dict = transfer_list2dict(test_list)
 
 del train_list, test_list, data
 gc.collect()
+
+
+# %% Item Similarity.
+def item_similarity(train: Dict[int, Dict[int, int]]):
+    # Calculate co-rated users between items.
+    C: Dict[int, Dict[int, int]] = dict()
+    N: Dict[int, int] = dict()
+    for u, items in train.items():
+        for i in items.keys():
+            N[i] = N.get(i, 0) + 1
+            for j in items.keys():
+                if i == j:
+                    continue
+                else:
+                    if C.get(i):
+                        C[i][j] = C[i].get(j, 0) + 1
+                    else:
+                        C[i] = {j: 1}
+    pprint(C)
+
+    # Calculate finial similarity matrix W.
+    W: Dict[int, Dict[int, float]] = dict()
+    for i, related_items in C.items():
+        for j, cij in related_items.items():
+            if W.get(i):
+                W[i][j] = cij / math.sqrt(N[i] * N[j])
+            else:
+                W[i] = {j: cij / math.sqrt(N[i] * N[j])}
+    return W
+
+
+train_sample = {
+    1: {1: 1, 2: 1, 4: 1},
+    2: {2: 1, 3: 1, 5: 1},
+    3: {3: 1, 4: 1},
+    4: {2: 1, 3: 1, 4: 1},
+    5: {1: 1, 4: 1}
+}
+W_sample = item_similarity(train_sample)
