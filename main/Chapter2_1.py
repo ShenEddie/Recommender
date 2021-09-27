2f7ddba07f0752a811962bb273d04604bceaaabe
Eddie Shen <sheneddie@outlook.com>
2021-09-27 11:00:54 +0800

    Modify item similarity: add tqdm

--- main/Chapter2_1.py
+++ main/Chapter2_1.py
@@ -52,7 +52,7 @@ def item_similarity(train: Dict[int, Dict[int, int]]):
     # Calculate co-rated users between items.
     C: Dict[int, Dict[int, int]] = dict()
     N: Dict[int, int] = dict()
-    for u, items in train.items():
+    for u, items in tqdm(train.items()):
         for i in items.keys():
             N[i] = N.get(i, 0) + 1
             for j in items.keys():
@@ -67,7 +67,7 @@ def item_similarity(train: Dict[int, Dict[int, int]]):
 
     # Calculate finial similarity matrix W.
     W: Dict[int, Dict[int, float]] = dict()
-    for i, related_items in C.items():
+    for i, related_items in tqdm(C.items()):
         for j, cij in related_items.items():
             if W.get(i):
                 W[i][j] = cij / math.sqrt(N[i] * N[j])
@@ -76,11 +76,12 @@ def item_similarity(train: Dict[int, Dict[int, int]]):
     return W
 
 
-train_sample = {
-    1: {1: 1, 2: 1, 4: 1},
-    2: {2: 1, 3: 1, 5: 1},
-    3: {3: 1, 4: 1},
-    4: {2: 1, 3: 1, 4: 1},
-    5: {1: 1, 4: 1}
-}
-W_sample = item_similarity(train_sample)
+# train_sample = {
+#     1: {1: 1, 2: 1, 4: 1},
+#     2: {2: 1, 3: 1, 5: 1},
+#     3: {3: 1, 4: 1},
+#     4: {2: 1, 3: 1, 4: 1},
+#     5: {1: 1, 4: 1}
+# }
+# W_sample = item_similarity(train_sample)
+W = item_similarity(train_dict)
