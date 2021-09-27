3730c6561ebf8b0a3aaf630632df3751e8051799
Eddie Shen <sheneddie@outlook.com>
2021-09-27 14:37:35 +0800

    Add precision for itemcf

--- main/Chapter2_1.py
+++ main/Chapter2_1.py
@@ -123,3 +123,25 @@ def recall_item_cf(train: Dict[int, Dict[int, int]],
 
 
 print(recall_item_cf(train_dict, test_dict, W, 10, 10))
+
+
+# %% Precision
+def precision_user_cf(train: Dict[int, Dict[int, int]],
+                      test: Dict[int, Dict[int, int]],
+                      W: Dict[int, Dict[int, float]],
+                      k: int,
+                      n: int) -> float:
+    hit = 0
+    all = 0
+    for user in tqdm(train.keys()):
+        tu = test.get(user, {})
+        rank = recommend_item_cf(user, train, W, k)
+        rank = sorted(rank.items(), key=itemgetter(1), reverse=True)[0:n]
+        for item, pui in rank:
+            if item in tu:
+                hit += 1
+        all += len(rank)
+    return hit / all
+
+
+print(precision_user_cf(train_dict, test_dict, W, 80, 10))
