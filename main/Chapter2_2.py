b7b0d13d41dfd05ca1c746cc432c8de5f6b32226
Eddie Shen <sheneddie@outlook.com>
2021-09-28 14:32:35 +0800

    Add items_pool for LFM

--- main/Chapter2_2.py
+++ main/Chapter2_2.py
@@ -6,3 +6,28 @@
 # @Email   : sheneddie@outlook.com
 # @File    : Chapter2_2.py
 # @Software: PyCharm
+
+
+# %% Import packages.
+import sys
+import gc
+import math
+import random
+from typing import List, Dict
+from operator import itemgetter
+from tqdm import tqdm
+
+sys.path.append('../')
+try:
+    from utils.load_data import load_m1_1m, transfer_list2dict
+except ModuleNotFoundError:
+    raise
+
+
+# %% Get items' pool
+def items_pool(data: List[List[int]]) -> List[int]:
+    return [line[1] for line in data].copy()
+
+
+data = load_m1_1m()
+items_pool = items_pool(data)
