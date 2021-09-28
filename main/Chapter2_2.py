fb7f1e3a6f0b3c4857a802f57186d5efb3a5ce7b
Eddie Shen <sheneddie@outlook.com>
2021-09-28 14:43:40 +0800

    Add selection of negative samples for LFM

--- main/Chapter2_2.py
+++ main/Chapter2_2.py
@@ -31,3 +31,22 @@ def items_pool(data: List[List[int]]) -> List[int]:
 
 data = load_m1_1m()
 items_pool = items_pool(data)
+
+
+# %% Select negative sample:
+def random_select_negative_sample(items: Dict[int, int],
+                                  items_pool: List[int]) -> Dict[int, int]:
+    ret = dict()
+    for i in items.keys():
+        ret[i] = 1  # Positive sample.
+    n = 0
+    for i in range(0, len(items) * 3):
+        item = random.choice(items_pool)
+        if item in ret:
+            continue
+        else:
+            ret[item] = 0  # Negative sample.
+            n += 1
+            if n >= len(items):  # To make sure positive & negative samples are close.
+                break
+    return ret
