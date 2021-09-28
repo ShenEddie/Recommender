0430d437d0777981c3bf0e642368ee6f328f2272
Eddie Shen <sheneddie@outlook.com>
2021-09-28 12:07:21 +0800

    Add file for LFM model

---
+++ main/Chapter2_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# @Time    : 2021-09-28 12:06
+# @Author  : Eddie Shen
+# @Email   : sheneddie@outlook.com
+# @File    : Chapter2_2.py
+# @Software: PyCharm
