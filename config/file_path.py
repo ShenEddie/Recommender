dba460a17c8f1179994d8580b361b5cb090084e7
Eddie Shen <sheneddie@outlook.com>
2021-09-29 10:25:08 +0800

    Modify file_path: add lfm params

--- config/file_path.py
+++ config/file_path.py
@@ -7,4 +7,8 @@
 # @File    : file_path.py
 # @Software: PyCharm
 
-ml_1m_ratings_path = r"D:\Eddie\Documents\BOSC\RecommendSystem\data\ml-1m\ratings.dat"
+import os.path
+
+pwd = r'D:\Eddie\Documents\BOSC\RecommendSystem'
+ml_1m_ratings_path = os.path.join(pwd, r'data/ml-1m/ratings.dat')
+lfm_params_path = os.path.join(pwd, r'params/P_Q_lfm.pkl')
